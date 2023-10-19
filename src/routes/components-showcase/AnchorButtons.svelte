7a697b7c5b2abddf7248874495769cdefdfea706
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2023-10-19 10:19:04 +0200

    Split components showcase in multiple files

---
+++ src/routes/components-showcase/AnchorButtons.svelte
@@ -0,0 +1,86 @@
+<script>
+	import { mdiHome } from "@mdi/js";
+
+	import { AnchorButton } from "$lib/dusk/components";
+</script>
+
+<section>
+	<AnchorButton
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		disabled={true}
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="secondary"
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="secondary"
+		disabled={true}
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="tertiary"
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="tertiary"
+		disabled={true}
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+</section>
+<section>
+	<AnchorButton
+		className="small"
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="small"
+		disabled={true}
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="secondary small"
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="secondary small"
+		disabled={true}
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="tertiary small"
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+	<AnchorButton
+		className="tertiary small"
+		disabled={true}
+		href="http://example.com"
+		icon={{ path: mdiHome }}
+		text="Anchor text"
+	/>
+</section>
---
+++ src/routes/components-showcase/LabeledButtons.svelte
@@ -0,0 +1,80 @@
+<script>
+	import { mdiHome } from "@mdi/js";
+
+	import { Button } from "$lib/dusk/components";
+</script>
+
+<section>
+	<Button
+		icon={{ path: mdiHome }}
+		tabindex=""
+		text="Some text"
+	/>
+	<Button
+		disabled={true}
+		icon={{ path: mdiHome }}
+		tabindex=""
+		text="Some text"
+	/>
+	<Button
+		className="secondary"
+		icon={{ path: mdiHome }}
+		tabindex=""
+		text="Some text"
+	/>
+	<Button
+		className="secondary"
+		disabled={true}
+		icon={{ path: mdiHome }}
+		tabindex=""
+		text="Some text"
+	/>
+	<Button
+		className="tertiary"
+		icon={{ path: mdiHome }}
+		tabindex=""
+		text="Some text"
+	/>
+	<Button
+		className="tertiary"
+		disabled={true}
+		icon={{ path: mdiHome }}
+		tabindex=""
+		text="Some text"
+	/>
+</section>
+<section>
+	<Button
+		className="small"
+		icon={{ path: mdiHome }}
+		text="Some text"
+	/>
+	<Button
+		className="small"
+		disabled={true}
+		icon={{ path: mdiHome }}
+		text="Some text"
+	/>
+	<Button
+		className="secondary small"
+		icon={{ path: mdiHome }}
+		text="Some text"
+	/>
+	<Button
+		className="secondary small"
+		disabled={true}
+		icon={{ path: mdiHome }}
+		text="Some text"
+	/>
+	<Button
+		className="tertiary small"
+		icon={{ path: mdiHome }}
+		text="Some text"
+	/>
+	<Button
+		className="tertiary small"
+		disabled={true}
+		icon={{ path: mdiHome }}
+		text="Some text"
+	/>
+</section>
