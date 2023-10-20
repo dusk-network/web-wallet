ce0e8af6ca1258ea98364bee9fa0338725379ab6
Norton Andreev <norton@nortonandreev.com>
2023-10-20 16:16:27 +0100

    Allow passing external classes to Card component

--- src/lib/dusk/components/Card/Card.svelte
+++ src/lib/dusk/components/Card/Card.svelte
@@ -1,21 +1,27 @@
 <script>
 	import Icon from "../Icon/Icon.svelte";
+	import { makeClassName } from "$lib/dusk/string";
+
+	/** @type {String | Undefined} */
+	export let className = undefined;
 
 	/** @type {string | Undefined} */
 	export let iconPath = undefined;
 
 	/** @type {string} */
 	export let heading;
+
+	$: classes = makeClassName(["dusk-card", className]);
 </script>
 
-<div {...$$restProps} class="dusk-card">
+<div {...$$restProps} class={classes}>
 	<div class="dusk-card__header">
 		{#if iconPath}
 			<div class="dusk-card__icon">
-				<Icon path={iconPath}/>
+				<Icon path={iconPath} />
 			</div>
 		{/if}
 		<p>{heading}</p>
 	</div>
-	<slot/>
+	<slot />
 </div>
