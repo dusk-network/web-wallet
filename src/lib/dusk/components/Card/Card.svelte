c8d2416cd586fa45943582da6d91acf0c67ec125
Norton Andreev <norton@nortonandreev.com>
2023-10-20 16:22:23 +0100

    Implement Card component
    Closes #10

---
+++ src/lib/dusk/components/Card/Card.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Icon from "../Icon/Icon.svelte";
+	import { makeClassName } from "$lib/dusk/string";
+
+	/** @type {String | Undefined} */
+	export let className = undefined;
+
+	/** @type {string | Undefined} */
+	export let iconPath = undefined;
+
+	/** @type {string} */
+	export let heading;
+
+	$: classes = makeClassName(["dusk-card", className]);
+</script>
+
+<div {...$$restProps} class={classes}>
+	<div class="dusk-card__header">
+		{#if iconPath}
+			<div class="dusk-card__icon">
+				<Icon path={iconPath}/>
+			</div>
+		{/if}
+		<p>{heading}</p>
+	</div>
+	<slot/>
+</div>
