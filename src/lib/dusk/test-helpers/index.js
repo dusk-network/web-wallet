a9660f9cd9e46f0ffc10eb60111af36bbaa115da
Andrea Scartabelli <andrea.scartabelli@gmail.com>
2023-11-07 15:44:33 +0100

    Add easier tab scroll interactions for mobile devices
    Resolves #13

--- src/lib/dusk/test-helpers/index.js
+++ src/lib/dusk/test-helpers/index.js
@@ -1,3 +1,4 @@
+export { default as getAsHTMLElement } from "./getAsHTMLElement";
 export { default as renderWithSimpleContent } from "./renderWithSimpleContent";
 export { default as renderWithSlots } from "./renderWithSlots";
 export { default as SlotContent } from "./SlotContent.svelte";
