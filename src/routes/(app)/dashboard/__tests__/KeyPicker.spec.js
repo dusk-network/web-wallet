21d6a46ab5c1621cb9e1df989eb70114425c3fa0
Norton Andreev <norton@nortonandreev.com>
2023-12-07 15:06:39 +0200

    Resolve KeyPicker visual issues
    Resolves #118, #134

--- src/routes/(app)/dashboard/__tests__/KeyPicker.spec.js
+++ src/routes/(app)/dashboard/__tests__/KeyPicker.spec.js
@@ -1,5 +1,5 @@
-import { afterEach, describe, expect, it, vitest } from "vitest";
-import { cleanup, fireEvent, render } from "@testing-library/svelte";
+import { afterEach, describe, expect, it } from "vitest";
+import { cleanup, render } from "@testing-library/svelte";
 import KeyPicker from "../KeyPicker.svelte";
 
 describe("KeyPicker", () => {
@@ -18,44 +18,5 @@ describe("KeyPicker", () => {
 		const { container } = render(KeyPicker, props);
 
 		expect(container.firstChild).toMatchSnapshot();
-	});
-
-	it("toggles dropdown on click", async () => {
-		const { getByRole, queryByText } = render(KeyPicker, props);
-
-		const component = getByRole("button");
-
-		await fireEvent.click(component);
-		expect(queryByText(keys[1])).toBeInTheDocument();
-		await fireEvent.click(component);
-		expect(queryByText(keys[1])).not.toBeInTheDocument();
-	});
-
-	it("updates selected key on click", async () => {
-		const { getByRole, getByText } = render(KeyPicker, props);
-		const componentTrigger = getByRole("button");
-
-		await fireEvent.click(componentTrigger);
-
-		const secondKeyOption = getByText(keys[1]);
-
-		await fireEvent.click(secondKeyOption);
-		expect(getByText(keys[1])).toBeInTheDocument();
-	});
-
-	it("dispatches generateKey event on button click", async () => {
-		const { getByRole, getByText, component } = render(KeyPicker, props);
-		const spy = vitest.fn();
-
-		const componentTrigger = getByRole("button");
-
-		await fireEvent.click(componentTrigger);
-
-		component.$on("generateKey", spy);
-
-		const generateKeyButton = getByText("Generate Key");
-
-		await fireEvent.click(generateKeyButton);
-		expect(spy).toHaveBeenCalledTimes(1);
 	});
 });
