b084d52b976be769ea26c4b8536cdd2d2631eb32
Norton Andreev <norton@nortonandreev.com>
2024-01-11 16:23:15 +0200

    Update the Wallet instance initialisation
    Resolves #414

--- src/lib/services/wallet/getWallet.js
+++ src/lib/services/wallet/getWallet.js
@@ -9,6 +9,6 @@ import { Wallet } from "@dusk-network/dusk-wallet-js";
  * @returns {Wallet}
  */
 
-const getWallet = (seed, gasLimit, gasPrice) => new Wallet(seed, gasLimit, gasPrice);
+const getWallet = (seed, gasLimit, gasPrice) => new Wallet(Array.from(seed), gasLimit, gasPrice);
 
 export default getWallet;
