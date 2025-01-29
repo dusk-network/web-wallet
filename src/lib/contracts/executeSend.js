dusk-network/rusk

web-wallet: Add memo field in send flow
Resolves #3112

web-wallet/src/lib/contracts/executeSend.js
@@ -3,8 +3,8 @@ import { Gas } from "$lib/vendor/w3sper.js/src/mod";
 
 import { walletStore } from "$lib/stores";
 
-/** @type {(to: string, amount: bigint, gasPrice: bigint, gasLimit: bigint) => Promise<string>} */
-const executeSend = (to, amount, gasPrice, gasLimit) => {
+/** @type {(to: string, amount: bigint, gasPrice: bigint, gasLimit: bigint, memo: any) => Promise<string>} */
+const executeSend = (to, amount, gasPrice, gasLimit, memo) => {
   return walletStore
     .transfer(
       to,
@@ -12,7 +12,8 @@ const executeSend = (to, amount, gasPrice, gasLimit) => {
       new Gas({
         limit: gasLimit,
         price: gasPrice,
-      })
+      }),
+      memo
     )
     .then(getKey("hash"));
 };

web-wallet/src/lib/stores/walletStore.js
@@ -374,7 +374,7 @@ async function sync(fromBlock) {
 }
 
 /** @type {WalletStoreServices["transfer"]} */
-const transfer = async (to, amount, gas) =>
+const transfer = async (to, amount, gas, memo) =>
   sync()
     .then(networkStore.connect)
     .then((network) => {
@@ -382,6 +382,8 @@ const transfer = async (to, amount, gas) =>
         .as(getCurrentProfile())
         .transfer(amount)
         .to(to)
+        // @ts-ignore we don't have access to the AddressTransfer type
+        .memo(memo)
         .gas(gas);
 
       return network.execute(

dusk-network/web-wallet

src/lib/contracts/executeSend.js
@@ -3,12 +3,13 @@ import { Gas } from "$lib/vendor/w3sper.js/src/mod";
 
 import { walletStore } from "$lib/stores";
 
-/** @type {(to: string, amount: bigint, gasPrice: bigint, gasLimit: bigint) => Promise<string>} */
-const executeSend = (to, amount, gasPrice, gasLimit) => {
+/** @type {(to: string, amount: bigint, memo: string, gasPrice: bigint, gasLimit: bigint) => Promise<string>} */
+const executeSend = (to, amount, memo, gasPrice, gasLimit) => {
   return walletStore
     .transfer(
       to,
       amount,
+      memo,
       new Gas({
         limit: gasLimit,
         price: gasPrice,

src/lib/stores/walletStore.js
@@ -380,7 +380,7 @@ async function sync(fromBlock) {
 }
 
 /** @type {WalletStoreServices["transfer"]} */
-const transfer = async (to, amount, gas) =>
+const transfer = async (to, amount, memo, gas) =>
   sync()
     .then(networkStore.connect)
     .then((network) => {
@@ -388,6 +388,7 @@ const transfer = async (to, amount, gas) =>
         .as(getCurrentProfile())
         .transfer(amount)
         .to(to)
+        .memo(memo)
         .gas(gas);
 
       return network.execute(